998b60856339d61a9174d6e106cda66b3a6bc325
Marina Barabash <mb974343@gmail.com>
2026-01-22 12:27:04 +0000

    new commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,7 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/Log",
     "sap/bpt/helloworldui5/model/models"
-], function (UIComponent, Log, models) {
+], function (UIComponent, models) {
     "use strict";
 
     return UIComponent.extend("sap.bpt.helloworldui5.Component", {
@@ -13,11 +12,11 @@ sap.ui.define([
         init: function () {
             UIComponent.prototype.init.apply(this, arguments);
 
-            Log.info("Hello-World Information Log");
-            Log.debug("Hello-World Debug Log");
-            Log.warning("Hello-World Warning Log");
-            Log.error("Hello-World Error Log");
-            Log.fatal("Hello-World Fatal Log");
+            console.info("Hello-World Information Log");
+            console.debug("Hello-World Debug Log");
+            console.warn("Hello-World Warning Log");
+            console.error("Hello-World Error Log");
+
 
             this.setModel(models.createDeviceModel(), "device");
             this.getRouter().initialize();
